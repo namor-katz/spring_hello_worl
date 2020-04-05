80e0e4d35954bc0f5950c60fa704b4b2c10c6e37
RomanK <namor925@gmail.com>
2020-04-05 21:22:55 +0700

    final 5

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,9 +5,14 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        boolean eq2 = (bean == bean2);
 
-        Cat bean2 = (Cat)  applicationContext.getBean("cat");
-        System.out.println(bean2.getName());
+        Cat cat = (Cat)  applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        boolean eq = (cat == cat2);
+
+        System.out.println("правда ли, что cat это cat2? " + eq);
+        System.out.println("а хелловорд, конечно, один объект, и это " + eq2);
     }
 }
--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -1,7 +1,9 @@
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
+//import org.springframework.beans.factory.config.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
 
 @Configuration
 public class AppConfig {
@@ -13,8 +15,8 @@ public class AppConfig {
         return helloWorld;
     }
 
-//    @Scope("request")
     @Bean(name="cat")
+    @Scope("prototype")
     public Cat getCat() {
         Cat cat = new Cat("may", 1, 3);
         return cat;
